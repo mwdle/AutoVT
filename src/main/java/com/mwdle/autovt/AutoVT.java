5739463bb586e6e715e26390840b767e92c9c486
mwdle <mwdle@mwdle.com>
2025-01-21 02:05:22 -0700

    General speed improvements and some reliability improvements for captcha.

--- src/main/java/com/mwdle/autovt/AutoVT.java
+++ src/main/java/com/mwdle/autovt/AutoVT.java
@@ -26,9 +26,8 @@ public class AutoVT {
 
     // TODO: Setup checks for community scores for uploads.
 
-    public static final Duration UPLOAD_TIMEOUT = Duration.ofMinutes(2);
-    public static final Duration REPORT_TIMEOUT = Duration.ofMinutes(3);
-    public static final Duration CAPTCHA_TIMEOUT = Duration.ofMinutes(10);
+    public static final Duration REPORT_TIMEOUT = Duration.ofMinutes(3); // The maximum amount of time to wait for the file to upload, for the file to get scanned, and the report to load.
+    public static final Duration CAPTCHA_TIMEOUT = Duration.ofMinutes(20);
 
     public static final File currentDir = new File(System.getProperty("user.dir"));
     private static String detectionsReportFilepath;
@@ -48,18 +47,19 @@ public class AutoVT {
         Selenide.sleep(1500); // Wait for the page to load to determine if multi-factor authentication is required
         SelenideElement multiFactorAuth = $x("//*[contains(text(), 'Authentication code')]");
         if (multiFactorAuth.isDisplayed()) {
-            Toolkit.getDefaultToolkit().beep();
+            Toolkit.getDefaultToolkit().beep(); // Make a sound to notify the user that multi-factor authentication is required
             $(By.id("code2fa")).shouldHave(Condition.attributeMatching("value", "^[0-9]{6}$"), Duration.ofMinutes(5)); // Wait for up to 5 minutes for the user to enter the authentication code
             $(By.id("sign-in-btn")).shouldBe(interactable).click();
         }
     }
 
-    public static void appendResultToReport(String filename, String numberOfDetections) {
+    public static void appendResultToReport(File file, String numberOfDetections) {
         String base64Screenshot = Base64.getEncoder().encodeToString(Selenide.screenshot(OutputType.BYTES));
         boolean isClean = numberOfDetections.equals("No");
         try (FileWriter reportFileWriter = new FileWriter(isClean ? cleanReportFilepath : detectionsReportFilepath, true)) {
-            reportFileWriter.append("---\n\n### ").append(filename).append("\n")
-                    .append("There were ").append(numberOfDetections.toLowerCase()).append(" detections for: `").append(filename).append("`\n\n")
+            String pathOfFileToReport = file.getAbsolutePath();
+            reportFileWriter.append("---\n\n### ").append(file.getName()).append("\n")
+                    .append("There were ").append(numberOfDetections.toLowerCase()).append(" detections for: [").append(pathOfFileToReport).append("](<file:///").append(pathOfFileToReport).append(">)\n\n")
                     .append("[View Report](").append(Selenide.webdriver().driver().url()).append(")\n")
                     .append("![Screenshot of the report](").append("data:image/png;base64,").append(base64Screenshot).append(")\n\n");
         }
@@ -68,26 +68,25 @@ public class AutoVT {
         }
     }
 
-    public static void checkForMaliciousResult(String filename) {
+    public static void collectResultsForReport(File file) {
         SelenideElement reportSummary = $(shadowCss("div > div.card-header > div.fw-bold", "#view-container > file-view", "#report > vt-ui-file-card")).should(exist, Duration.ofSeconds(10)); // Wait for the report to load
         String regex = "(No|\\d+\\/\\d+) security vendors flagged this file as malicious$";
         Matcher match = Pattern.compile(regex).matcher(reportSummary.getText());
         if (!match.find()) throw new RuntimeException("Upload result not found.");
-        appendResultToReport(filename, match.group(1));
+        appendResultToReport(file, match.group(1));
     }
 
     public static boolean checkForCaptcha() {
-        Selenide.sleep(1250);
         SelenideElement captcha = $x("//*[contains(@id, 'captcha')]/*");
-        boolean hasCaptcha = captcha.isDisplayed();
-        if (hasCaptcha) {
-            Toolkit.getDefaultToolkit().beep();
-            captcha.shouldNot(exist, CAPTCHA_TIMEOUT);
+        boolean captchaPresent = captcha.exists();
+        if (captchaPresent) {
+            Toolkit.getDefaultToolkit().beep(); // Make a sound to notify the user that a captcha is required
+            captcha.shouldNot(exist, CAPTCHA_TIMEOUT); // Wait for the desired amount of time or until the user enters the captcha
         }
-        return hasCaptcha;
+        return captchaPresent;
     }
 
-    public static void uploadFiles(String dirPath) {
+    public static void scanFilesAndGenerateReport(String dirPath) {
         SelenideElement uploadInput = $(shadowCss("#fileSelector", "#view-container > home-view", "#uploadForm")).should(exist);
         SelenideElement uploadButton = $(shadowCss("div > form > button", "#view-container > home-view", "#uploadForm"));
         File dir = new File(dirPath);
@@ -95,22 +94,35 @@ public class AutoVT {
         assert files != null;
         for (File file : files) {
             if (file.isDirectory()) {
-                uploadFiles(file.getAbsolutePath());
+                scanFilesAndGenerateReport(file.getAbsolutePath());
             } else {
                 uploadInput.uploadFile(file);
                 Selenide.sleep(1500);
                 if (uploadButton.has(text("Confirm Upload"))) uploadButton.click();
-                if (checkForCaptcha()) {
-                    Selenide.sleep(1500);
-                    if (uploadInput.exists()) {
-                        uploadInput.uploadFile(file);
+
+                SelenideElement report = $(shadowCss("#report", "#view-container > file-view"));
+
+                Duration currentTime = Duration.ZERO;
+
+                while (!report.exists()) {
+                    if (checkForCaptcha()) {
                         Selenide.sleep(1500);
-                        if (uploadButton.has(text("Confirm Upload"))) uploadButton.click();
+                        currentTime.plus(Duration.ofMillis(1500));
+                        if (uploadInput.exists()) { // Sometimes VT makes you reupload after captcha.
+                            uploadInput.uploadFile(file);
+                            Selenide.sleep(1500);
+                            currentTime.plus(Duration.ofMillis(1500));
+                            if (uploadButton.has(text("Confirm Upload"))) uploadButton.click();
+                        }
                     }
+                    else {
+                        Selenide.sleep(1000);
+                        currentTime.plus(Duration.ofSeconds(1));
+                    }
+                    if (currentTime.compareTo(REPORT_TIMEOUT) >= 0) throw new RuntimeException("Report not found after " + REPORT_TIMEOUT.getSeconds() + " seconds."); // If the report is not found within the desired amount of time, throw an exception
                 }
-                uploadButton.shouldNot(exist, UPLOAD_TIMEOUT);
-                $(shadowCss("#report", "#view-container > file-view")).should(exist, REPORT_TIMEOUT); // Wait for the report to generate
-                checkForMaliciousResult(file.getName());
+
+                collectResultsForReport(file);
                 Selenide.open("https://www.virustotal.com/");
             }
         }
@@ -123,12 +135,12 @@ public class AutoVT {
         File cleanReportFile = new File(currentDir.toString().replace(' ', '_'), (new File(dir)).getName() + "_clean_report_" + today + ".md");
         cleanReportFilepath = cleanReportFile.getPath();
         try (FileWriter reportFileWriter = new FileWriter(detectionsReportFile, false)) {
-            reportFileWriter.append("# Virus Total Report\n**This is a Virus Total report for all files that were flagged as malicious from the directory:** `").append(dir).append("`\n\n*This report was created on: ").append(today).append("*\n\n");
+            reportFileWriter.append("# Virus Total Report\n**This is a Virus Total report for all files that were flagged as malicious from the directory:** [").append(dir).append("](<file:///").append(dir).append(">)\n\n*This report was created on: ").append(today).append("*\n\n");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         try (FileWriter reportFileWriter = new FileWriter(cleanReportFile, false)) {
-            reportFileWriter.append("# Virus Total Report\n**This is a Virus Total report for all files that were not flagged by any security vendors from the directory:** `").append(dir).append("`\n\n*This report was created on: ").append(today).append("*\n\n");
+            reportFileWriter.append("# Virus Total Report\n**This is a Virus Total report for all files that were not flagged by any security vendors from the directory:** [").append(dir).append("](<file:///").append(dir).append(">)\n\n*This report was created on: ").append(today).append("*\n\n");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -138,14 +150,10 @@ public class AutoVT {
         initializeReports(dir);
         openVT();
         login(username, password);
-        uploadFiles(dir);
+        scanFilesAndGenerateReport(dir);
     }
 
     public static void main(String[] args) {
-        // Configure Chrome
-        ChromeOptions chromeOptions = new ChromeOptions();
-//        chromeOptions.addArguments("--incognito", "--headless", "--no-sandbox", "--disable-gpu", "--start-maximized", "--window-size=1920,1080");
-        Configuration.browserCapabilities = chromeOptions;
         // Handle input arguments
         if (args.length == 3)
             scanFiles(args[0], args[1], args[2]);
